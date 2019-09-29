f9bc2204bca53a9ad84de8f9ea7e154bb33dc258
Jan Müller <janmueller3698@gmail.com>
2019-09-29 12:56:07 +0200

    Expanded method library

--- src/main/kotlin/eu/yeger/kotlin/javafx/Fragments.kt
+++ src/main/kotlin/eu/yeger/kotlin/javafx/Fragments.kt
@@ -1,5 +1,6 @@
 package eu.yeger.kotlin.javafx
 
+import javafx.beans.binding.StringBinding
 import javafx.beans.property.StringProperty
 import javafx.scene.Node
 import javafx.scene.control.Button
@@ -14,11 +15,14 @@ import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
 import javafx.scene.shape.Rectangle
 
-class FragmentException(message: String) : Exception(message)
+// ===============================================================
+// Fragment
 
 open class Fragment<out T: Node>(private val builder: () -> T) {
     open fun instance() = builder()
 }
+
+class FragmentException(message: String) : Exception(message)
 
 class SingletonFragment<out T: Node>(private val node: T) : Fragment<T>({ node }) {
     private var consumed = false
@@ -31,11 +35,17 @@ class SingletonFragment<out T: Node>(private val node: T) : Fragment<T>({ node }
 
 fun <T: Node> T.asSingletonFragment() = SingletonFragment(this)
 
+// ===============================================================
+// Button
+
 inline fun button(crossinline init: @FXMarker Button.() -> Unit = {}) =
     Fragment { Button().apply(init) }
 
 inline fun button(text: String, crossinline init: @FXMarker Button.() -> Unit = {}) =
     Fragment { Button(text).apply(init) }
+
+// ===============================================================
+// Label
 
 inline fun label(crossinline init: @FXMarker Label.() -> Unit = {}) =
     Fragment { Label().apply(init) }
@@ -43,37 +53,106 @@ inline fun label(crossinline init: @FXMarker Label.() -> Unit = {}) =
 inline fun label(text: String, crossinline init: @FXMarker Label.() -> Unit = {}) =
     Fragment { Label(text).apply(init) }
 
-    inline fun label(binding: StringProperty, crossinline init: @FXMarker Label.() -> Unit = {}) =
-        Fragment {
-            Label().apply {
-                textProperty().bind(binding)
-                init()
-            }
+inline fun label(binding: StringProperty, crossinline init: @FXMarker Label.() -> Unit = {}) =
+    Fragment {
+        Label().apply {
+            textProperty().bind(binding)
+            init()
         }
+    }
 
-    inline fun textField(crossinline init: @FXMarker TextField.() -> Unit = {}) =
-        Fragment { TextField().apply(init) }
+inline fun label(binding: StringBinding, crossinline init: @FXMarker Label.() -> Unit = {}) =
+    Fragment {
+        Label().apply {
+            textProperty().bind(binding)
+            init()
+        }
+    }
 
-    inline fun passwordField(crossinline init: @FXMarker PasswordField.() -> Unit = {}) =
-        Fragment { PasswordField().apply(init) }
+// ===============================================================
+// TextField
 
-    inline fun imageView(crossinline init: @FXMarker ImageView.() -> Unit = {}) =
-        Fragment { ImageView().apply(init) }
+inline fun textField(crossinline init: @FXMarker TextField.() -> Unit = {}) =
+    Fragment { TextField().apply(init) }
 
-    inline fun imageView(image: Image, crossinline init: @FXMarker ImageView.() -> Unit = {}) =
-        Fragment { ImageView(image).apply(init) }
+inline fun textField(binding: StringProperty, crossinline init: @FXMarker TextField.() -> Unit = {}) =
+    Fragment {
+        TextField().apply {
+            textProperty().bindBidirectional(binding)
+            init()
+        }
+    }
 
-    inline fun rectangle(crossinline init: @FXMarker Rectangle.() -> Unit = {}) =
-        Fragment { Rectangle().apply(init) }
+inline fun textField(binding: StringBinding, crossinline init: @FXMarker TextField.() -> Unit = {}) =
+    Fragment {
+        TextField().apply {
+            textProperty().bind(binding)
+            init()
+        }
+    }
 
-    inline fun hBox(crossinline init: @FXMarker HBox.() -> Unit = {}) =
-        Fragment { HBox().apply(init) }
+// ===============================================================
+// PasswordField
 
-    inline fun vBox(crossinline init: @FXMarker VBox.() -> Unit = {}) =
-        Fragment { VBox().apply(init) }
+inline fun passwordField(crossinline init: @FXMarker PasswordField.() -> Unit = {}) =
+    Fragment { PasswordField().apply(init) }
 
-    inline fun gridPane(crossinline init: @FXMarker GridPane.() -> Unit = {}) =
-        Fragment { GridPane().apply(init) }
+inline fun passwordField(binding: StringProperty, crossinline init: @FXMarker PasswordField.() -> Unit = {}) =
+    Fragment {
+        PasswordField().apply {
+            textProperty().bindBidirectional(binding)
+            init()
+        }
+    }
 
-    inline fun stackPane(crossinline init: @FXMarker StackPane.() -> Unit = {}) =
-        Fragment { StackPane().apply(init) }
+inline fun passwordField(binding: StringBinding, crossinline init: @FXMarker PasswordField.() -> Unit = {}) =
+    Fragment {
+        PasswordField().apply {
+            textProperty().bind(binding)
+            init()
+        }
+    }
+
+// ===============================================================
+// ImageView
+
+inline fun imageView(crossinline init: @FXMarker ImageView.() -> Unit = {}) =
+    Fragment { ImageView().apply(init) }
+
+inline fun imageView(image: Image, crossinline init: @FXMarker ImageView.() -> Unit = {}) =
+    Fragment { ImageView(image).apply(init) }
+
+// ===============================================================
+// Rectangle
+
+inline fun rectangle(crossinline init: @FXMarker Rectangle.() -> Unit = {}) =
+    Fragment { Rectangle().apply(init) }
+
+inline fun rectangle(width: Double, height: Double, crossinline init: @FXMarker Rectangle.() -> Unit = {}) =
+    Fragment { Rectangle(width, height).apply(init) }
+
+inline fun rectangle(size: Double, crossinline init: @FXMarker Rectangle.() -> Unit = {}) = rectangle(size, size, init)
+
+// ===============================================================
+// HBox
+
+inline fun hBox(crossinline init: @FXMarker HBox.() -> Unit = {}) =
+    Fragment { HBox().apply(init) }
+
+// ===============================================================
+// VBox
+
+inline fun vBox(crossinline init: @FXMarker VBox.() -> Unit = {}) =
+    Fragment { VBox().apply(init) }
+
+// ===============================================================
+// GridPane
+
+inline fun gridPane(crossinline init: @FXMarker GridPane.() -> Unit = {}) =
+    Fragment { GridPane().apply(init) }
+
+// ===============================================================
+// StackPane
+
+inline fun stackPane(crossinline init: @FXMarker StackPane.() -> Unit = {}) =
+    Fragment { StackPane().apply(init) }
